af04d8b8b10377650bb090192a3bb893edb7c74f
mazahako <kstja-udvichenk@rambler.ru>
2017-05-28 20:09:38 +0300

    fixed login page

--- js/authApp.js
+++ js/authApp.js
@@ -1,9 +1,3 @@
 (function() {
 	var app = angular.module("AuthApp", ['ngMaterial']);
-	app.config(function($mdThemingProvider) {
-		$mdThemingProvider.theme('altTheme')
-			.primaryPalette('indigo')
-        $mdThemingProvider.theme('default')
-			.primaryPalette('teal')
-	});
 })();
