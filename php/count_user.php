76898c4515f25e4e3a77cc4477748fa2877ac4f0
Mishaaa7 <mishaaaghilenko@gmail.com>
2017-06-04 16:15:05 +0300

    added count_user and page

---
+++ php/count_user.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 04.06.2017
+ * Time: 14:44
+ */
+function autoload($class) {
+    require_once("../".strtolower($class).".php");
+}
+spl_autoload_register('autoload');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = database::get_instance();
+    $pg_con = $db->get_connection();
+    $count = pg_query($pg_con, "SELECT count(*) FROM myschema.users");
+    $count = pg_fetch_row($count)[0];
+    echo json_encode(array("count" => $count));
+}
---
+++ php/users.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 04.06.2017
+ * Time: 15:21
+ */
+function autoload($class) {
+    require_once("../".strtolower($class).".php");
+}
+spl_autoload_register('autoload');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $json = file_get_contents("php://input");
+    //$json = '{"page": 1}';
+    $page = json_decode($json, true);
+    $page = $page["page"];
+    $max = $page*20;
+    $min = $max-20;
+    $db = database::get_instance();
+    $pg_con = $db->get_connection();
+    pg_prepare($pg_con, "page", 'SELECT (id_user, firstname, surname) FROM myschema.users WHERE id_user >= $1 AND id_user <= $2');
+    $result = pg_execute($pg_con, "page", array($min, $max));
+    $result = json_encode(pg_fetch_all($result), JSON_UNESCAPED_UNICODE);
+    echo $result;
+}
